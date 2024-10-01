493b4bab15dae3c3df4b62f938bd63ebd578c373
smallproject <j.a.villas@live.com.ph>
2024-10-01 11:39:18 +0200

    added wall bracket output dto and a mapper

---
+++ src/main/java/nl/smallproject/www/techiteasy/dtos/WallBracketOutputDto.java
@@ -0,0 +1,49 @@
+package nl.smallproject.www.techiteasy.dtos;
+
+public class WallBracketOutputDto {
+    private long id;
+    private String size;
+    private boolean adjustable;
+    private String name;
+    private double price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public boolean isAdjustable() {
+        return adjustable;
+    }
+
+    public void setAdjustable(boolean adjustable) {
+        this.adjustable = adjustable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/nl/smallproject/www/techiteasy/mappers/WallBracketMapper.java
@@ -0,0 +1,25 @@
+package nl.smallproject.www.techiteasy.mappers;
+
+import nl.smallproject.www.techiteasy.dtos.WallBracketInputDto;
+import nl.smallproject.www.techiteasy.dtos.WallBracketOutputDto;
+import nl.smallproject.www.techiteasy.models.WallBracket;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WallBracketMapper {
+
+    public WallBracket wallBracketInputDtoToEntity(WallBracketInputDto wallBracketInputDto) {
+        WallBracket wallBracket = new WallBracket();
+        return wallBracket;
+    }
+
+    public WallBracketOutputDto wallBracketEntityToOutputDto(WallBracket wallBracket) {
+        WallBracketOutputDto wallBracketOutputDto = new WallBracketOutputDto();
+        wallBracketOutputDto.setId(wallBracket.getId());
+        wallBracketOutputDto.setSize(wallBracket.getSize());
+        wallBracketOutputDto.setAdjustable(wallBracket.getAdjustable());
+        wallBracketOutputDto.setName(wallBracket.getName());
+        wallBracketOutputDto.setPrice(wallBracket.getPrice());
+        return wallBracketOutputDto;
+    }
+}
